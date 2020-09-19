f0a19234d9a6e1243021b7a87e79d3be6962c6d3
sayyr1 <edwinsay03@gmail.com>
2020-09-19 12:36:06 -0500

    edited

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let daynames= [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
 let months= [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-let d = newDate();
+let d = new Date();
 let dayName= daynames[d.getDay()];
 let monthName= months[d.getMonth()];
 let year= d.getFullYear();
